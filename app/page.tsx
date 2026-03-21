9e7b8c48ba502bd105be1c288b11e661dbcdaaf1
Bobby Slattery <bobbyslattery@Bobby-Slattery-2021-Macbook-Pro.local>
2026-03-20 23:57:09 -0400

    Fix brewery logos - show in original colors on white backgrounds

--- app/page.tsx
+++ app/page.tsx
@@ -196,13 +196,13 @@ ${senderZip || "[Your Zip]"}, Ohio`;
 
         <div className="max-w-4xl mx-auto px-6 py-10 relative z-10">
           {/* Brewery logos */}
-          <div className="flex justify-center items-center gap-5 md:gap-8 mb-6 flex-wrap">
-            <img src="https://rhinegeist.com/wp-content/themes/wordplate/img/logo.svg" alt="Rhinegeist" className="h-10 md:h-12 brightness-0 invert" />
-            <img src="https://www.greatlakesbrewing.com/wp-content/uploads/2026/03/glbc-logo-blue-large.svg" alt="Great Lakes Brewing" className="h-10 md:h-12 brightness-0 invert" />
-            <img src="https://fiftywestbrew.com/wp-content/uploads/sites/90/2018/04/50W-Logo.png" alt="Fifty West" className="h-10 md:h-12 brightness-0 invert" />
-            <img src="https://craftpeak-cooler-images.imgix.net/jackie-os-pub-brewery/Logo_Outline.png" alt="Jackie O's" className="h-10 md:h-12 brightness-0 invert" />
-            <img src="https://artifactbeer.com/cdn/shop/files/UA2023-web-01_1000x.png" alt="Urban Artifact" className="h-10 md:h-12 brightness-0 invert" />
-            <img src="https://static1.squarespace.com/static/51abeb0be4b08f6a770c06bf/t/682c9b3ab03574795392d2be/1747753786428/SeventhSon_Logo_Orange_Shield.png" alt="Seventh Son" className="h-10 md:h-12 brightness-0 invert" />
+          <div className="flex justify-center items-center gap-3 md:gap-4 mb-6 flex-wrap">
+            <div className="bg-white rounded-xl px-3 py-2 shadow-md"><img src="https://rhinegeist.com/wp-content/themes/wordplate/img/logo.svg" alt="Rhinegeist" className="h-8 md:h-10" /></div>
+            <div className="bg-white rounded-xl px-3 py-2 shadow-md"><img src="https://www.greatlakesbrewing.com/wp-content/uploads/2026/03/glbc-logo-blue-large.svg" alt="Great Lakes Brewing" className="h-8 md:h-10" /></div>
+            <div className="bg-white rounded-xl px-3 py-2 shadow-md"><img src="https://fiftywestbrew.com/wp-content/uploads/sites/90/2018/04/50W-Logo.png" alt="Fifty West" className="h-8 md:h-10" /></div>
+            <div className="bg-white rounded-xl px-3 py-2 shadow-md"><img src="https://craftpeak-cooler-images.imgix.net/jackie-os-pub-brewery/Logo_Outline.png" alt="Jackie O's" className="h-8 md:h-10" /></div>
+            <div className="bg-white rounded-xl px-3 py-2 shadow-md"><img src="https://artifactbeer.com/cdn/shop/files/UA2023-web-01_1000x.png" alt="Urban Artifact" className="h-8 md:h-10" /></div>
+            <div className="bg-white rounded-xl px-3 py-2 shadow-md"><img src="https://static1.squarespace.com/static/51abeb0be4b08f6a770c06bf/t/682c9b3ab03574795392d2be/1747753786428/SeventhSon_Logo_Orange_Shield.png" alt="Seventh Son" className="h-8 md:h-10" /></div>
           </div>
 
           <div className="text-center mb-8">
